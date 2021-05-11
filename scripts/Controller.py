Add visualization feet trajectory + fix rotation

scripts/Controller.py
@@ -18,7 +18,7 @@ from solo3D.tools.HeightMap import HeightMap
 from solo3D.StatePlanner import StatePlanner
 from solo3D.LoggerPlanner import LoggerPlanner
 
-# from solo3D.tools.vizualization import PybVisualizationTraj
+from solo3D.tools.vizualization import PybVisualizationTraj
 
 class Result:
     """Object to store the result of the control loop
@@ -197,7 +197,7 @@ class Controller:
         dDevice.b_baseVel = np.zeros(3)
 
         # Load Heightmap
-        path_ = "solo3D/objects/object_4/heightmap/"
+        path_ = "solo3D/objects/object_5/heightmap/"
         surface_margin = 0.05
         self.heightMap = HeightMap(path_ , surface_margin)
 
@@ -210,7 +210,7 @@ class Controller:
         self.statePlanner = StatePlanner(dt_mpc, T_mpc, self.h_ref , self.heightMap)
         
         # Pybullet Trajectory 
-        # self.pybVisualizationTraj = PybVisualizationTraj(self.footTrajectoryGenerator)
+        self.pybVisualizationTraj = PybVisualizationTraj(self.gaitPlanner , self.footStepPlannerQP ,self.statePlanner ,  self.footTrajectoryGenerator , enable_pyb_GUI)
 
         # Log values for planner
         self.loggerPlanner = LoggerPlanner(dt_mpc , N_SIMULATION , T_gait , k_mpc)
@@ -328,14 +328,13 @@ class Controller:
         targetFootstep = self.footStepPlannerQP.computeTargetFootstep(self.k,self.q[0:7, 0:1], self.v[0:6, 0:1].copy(), o_v_ref)
 
         # Compute foot trajectory
-        self.footTrajectoryGenerator.update( self.k , targetFootstep, device , self.q , self.v)
+        self.footTrajectoryGenerator.update( self.k , targetFootstep, device , self.q , self.v)        
 
         self.statePlanner.computeReferenceStates(self.q[0:7, 0:1], self.v[0:6, 0:1].copy(), o_v_ref, 0.0)
 
         xref = self.statePlanner.getReferenceStates()
         fsteps = self.footStepPlannerQP.getFootsteps()
         cgait = self.gaitPlanner.getCurrentGait()
-
 
         t_planner = time.time()
 
@@ -417,7 +416,8 @@ class Controller:
         self.security_check()
 
         # Update PyBullet camera
-        self.pyb_camera(device)
+        # self.pyb_camera(device)
+        self.pybVisualizationTraj.update(self.k , device)
 
         # Logs
         self.log_misc(t_start, t_filter, t_planner, t_mpc, t_wbc)

scripts/solo3D/FootStepPlannerQP.py
@@ -96,7 +96,7 @@ class FootStepPlannerQP:
 
         # current position without height
         RPY_tmp = np.zeros(3)
-        q_tmp = q[:3,0]
+        q_tmp = q[:3,0].copy()
         q_tmp[2] = 0.
 
         for indice_L ,[i,j,indice_s , nb_ineq] in enumerate(L) :
@@ -147,7 +147,7 @@ class FootStepPlannerQP:
 
         
         # Store the value for getRefState
-        self.o_vref_qp = o_vref
+        self.o_vref_qp = o_vref.copy()
         self.o_vref_qp[0,0] = res[0]
         self.o_vref_qp[1,0] = res[1]        
 
@@ -164,7 +164,7 @@ class FootStepPlannerQP:
             q_dxdy = np.array( [self.dx[i - 1], self.dy[i - 1], 0.0 ])
 
             # Get future desired position of footsteps without k_feedback
-            nextFootstep = self.computeNextFootstep(i, j, b_vlin , b_vref_qp , True)
+            nextFootstep = self.computeNextFootstep(i, j, b_vlin , o_vref[:,0] , True)
 
             # Get desired position of footstep compared to current position
             RPY_tmp[2] = self.yaws[i-1]
@@ -256,7 +256,7 @@ class FootStepPlannerQP:
         if o_vref[5,0] != 0 :
             for j in range(1,len(self.footsteps) ) :
                 self.dx[j] = (v[0] * np.sin(o_vref[5,0] * self.dt_cum[j] ) + v[1] * (np.cos(o_vref[5,0] * self.dt_cum[j]) - 1.)   ) / o_vref[5,0]
-                self.dy[j] = (v[1] * np.sin(o_vref[5,0] * self.dt_cum[j] ) - v[0] * (np.sin(o_vref[5,0] * self.dt_cum[j]) - 1.)   ) / o_vref[5,0]
+                self.dy[j] = (v[1] * np.sin(o_vref[5,0] * self.dt_cum[j] ) - v[0] * (np.cos(o_vref[5,0] * self.dt_cum[j]) - 1.)   ) / o_vref[5,0]
         else :
             for j in range(1,len(self.footsteps) ) :
                 self.dx[j] = v[0]*self.dt_cum[j]
@@ -289,7 +289,7 @@ class FootStepPlannerQP:
                     self.footsteps[i][:,j] = self.footsteps[i-1][:,j]
             
             # Current position without height
-            q_tmp = q[:3,0]
+            q_tmp = q[:3,0].copy()
             q_tmp[2] = 0.
 
             # Feet that were in swing phase and are now in stance phase need to be updated
@@ -312,9 +312,18 @@ class FootStepPlannerQP:
 
                         if self.t0s[j] < 10e-4 and (self.k % self.k_mpc) == 0: 
                             #  beginning of flying phase, selection of surface
-                    
-                            height , id_surface = self.heightMap.getHeight(next_ft[0] , next_ft[1])
-                            self.surface_selected[j] = id_surface                        
+
+                            print("Next foot : " ,next_ft)
+                            
+                            
+                            id_surface = None
+                            for id_sf,sf in enumerate(self.heightMap.Surfaces) :
+                                if sf.isInsideIneq(next_ft) :
+                                    id_surface = id_sf
+                            # height , id_surface = self.heightMap.getHeight(next_ft[0] , next_ft[1])
+                            self.surface_selected[j] = id_surface  
+
+                            print("Surface selected : " , id_surface)                      
                      
                         id_surface = self.surface_selected[j] 
 
@@ -328,7 +337,11 @@ class FootStepPlannerQP:
 
                     else :   
                         # The surface can be modified at each time step if not in flying phase
-                        height , id_surface = self.heightMap.getHeight(next_ft[0] , next_ft[1])  
+                        # height , id_surface = self.heightMap.getHeight(next_ft[0] , next_ft[1]) 
+                        id_surface = None
+                        for id_sf,sf in enumerate(self.heightMap.Surfaces) :
+                            if sf.isInsideIneq(next_ft) :
+                                id_surface = id_sf 
                     
                         if id_surface != None :                 
                             
@@ -377,7 +390,7 @@ class FootStepPlannerQP:
         # self.nextFootstep[:,j] += self.shoulders[:,j] 
         
         # Taking into account Roll and Pitch (here base frame only takes yaw)
-        RP = self.RPY
+        RP = self.RPY.copy()
         RP[2] = 0.   # Remove Yaw, taking into account after
         nextFootstep += pin.rpy.rpyToMatrix(RP).dot( self.shoulders[:,j] ) 
 
@@ -403,6 +416,8 @@ class FootStepPlannerQP:
         self.RPY = pin.rpy.matrixToRpy(quat.toRotationMatrix())   # Array (3,)
         self.RPY_b = self.RPY.copy()
 
+        print("RPY : " , self.RPY  )
+
         # Only yaw rotation for the base frame
         self.RPY_b[0] = 0
         self.RPY_b[1] = 0
@@ -417,6 +432,12 @@ class FootStepPlannerQP:
 
         # Update desired location of footsteps on the ground
         self.updateTargetFootsteps()
+
+        # Update self.fsteps, not a list, but array representation (Nx12)
+        self.fsteps = np.zeros(( self.N_gait, 12  ))
+        
+        for index , footstep     in enumerate(self.footsteps):
+            self.fsteps[index , :] =  np.reshape(footstep, 12, order='F') 
 
         return self.getTargetFootsteps()
 
@@ -447,12 +468,11 @@ class FootStepPlannerQP:
         return 0
     
     def getFootsteps(self):
-        self.fsteps = np.zeros(( self.N_gait, 12  ))
-        
-        for index , footstep     in enumerate(self.footsteps):
-            self.fsteps[index , :] =  np.reshape(footstep, 12, order='F') 
 
         return self.fsteps
+
+    def getFootstepsList(self) :
+        return self.footsteps
 
     def getTargetFootsteps(self):
         return self.targetFootstep 

scripts/solo3D/StatePlanner.py
@@ -17,6 +17,13 @@ class StatePlanner() :
         # heightMap
         self.heightMap = heightMap 
 
+        # Surface equation : [a,b,c] : z = ax + by + c
+        # pitch  = - np.arctan2(a, 1.) ..
+        self.FIT_SIZE_X = 0.15
+        self.FIT_SIZE_Y = 0.15
+        self.surface_equation = np.zeros(3)
+        self.surface_point = np.zeros(3)
+
     
     def computeReferenceStates(self , q,  v, o_vref, z_average) : 
         '''
@@ -42,7 +49,7 @@ class StatePlanner() :
              # Displacement following the reference velocity compared to current position
             if o_vref[5,0] != 0 :               
                 self.referenceStates[0,i+1] = (o_vref[0,0] * np.sin(o_vref[5,0] * self.dt_vector[i] ) + o_vref[1,0] * (np.cos(o_vref[5,0] * self.dt_vector[i]) - 1.)   ) / o_vref[5,0]
-                self.referenceStates[1,i+1] = (o_vref[1,0] * np.sin(o_vref[5,0] * self.dt_vector[i] ) - o_vref[0,0] * (np.sin(o_vref[5,0] * self.dt_vector[i]) - 1.)   ) / o_vref[5,0]
+                self.referenceStates[1,i+1] = (o_vref[1,0] * np.sin(o_vref[5,0] * self.dt_vector[i] ) - o_vref[0,0] * (np.cos(o_vref[5,0] * self.dt_vector[i]) - 1.)   ) / o_vref[5,0]
             else :
                 
                 self.referenceStates[0,i+1] = o_vref[0,0]* self.dt_vector[i]
@@ -57,73 +64,78 @@ class StatePlanner() :
             self.referenceStates[6,i+1] =  o_vref[0,0] * np.cos(self.referenceStates[5,1 + i]) - o_vref[1,0] * np.sin(self.referenceStates[5,1 + i])
             self.referenceStates[7,i+1] =  o_vref[0,0] * np.sin(self.referenceStates[5,1 + i]) + o_vref[1,0] * np.cos(self.referenceStates[5,1 + i])
 
-            #self.referenceStates[5,i+1] += RPY[2]
+            self.referenceStates[5,i+1] += RPY[2]
 
             self.referenceStates[11,i+1] = o_vref[5,0] 
-
         
 
         ## Update according to heightmap
-        FIT_SIZE_X = 0.3
-        FIT_SIZE_Y = 0.1
+        
         Z_OFFSET = self.h_ref 
 
-        isInside , i_min, j_min = self.heightMap.find_nearest(q[0, 0:1] - 0.2*FIT_SIZE_X , q[1, 0:1]  - FIT_SIZE_Y )
-        isInside , i_max, j_max = self.heightMap.find_nearest(q[0, 0:1]  + FIT_SIZE_X , q[1, 0:1]  + FIT_SIZE_Y )
+        isInside_min , i_min, j_min = self.heightMap.find_nearest(q[0, 0:1] - self.FIT_SIZE_X , q[1, 0:1]  - self.FIT_SIZE_Y )
+        isInside_max , i_max, j_max = self.heightMap.find_nearest(q[0, 0:1]  + self.FIT_SIZE_X , q[1, 0:1]  + self.FIT_SIZE_Y )
 
-        nx = 20
-        ny = 2
-        X = np.linspace(i_min, i_max , nx).astype(int) 
-        Y = np.linspace(j_min, j_max , ny).astype(int) 
+        if isInside_min and isInside_max :
+            nx = 12
+            ny = 12
+            X = np.linspace(i_min, i_max , nx).astype(int) 
+            Y = np.linspace(j_min, j_max , ny).astype(int) 
 
-        n_points = nx * ny 
-        A = np.zeros((n_points, 3))
-        b = np.zeros(n_points)
-        i_pb = 0
-        for i in X:
-            for j in Y:
-                A[i_pb, :] = [self.heightMap.x[i], self.heightMap.y[j], 1.]
-                b[i_pb] = self.heightMap.heightMap[i, j][0]
-                i_pb += 1
+            n_points = nx * ny 
+            A = np.zeros((n_points, 3))
+            b = np.zeros(n_points)
+            i_pb = 0
+            for i in X:
+                for j in Y:
+                
+                    A[i_pb, :] = [self.heightMap.x[i], self.heightMap.y[j], 1.]
+                    b[i_pb] = self.heightMap.heightMap[i, j][0]
+                    i_pb += 1
 
-        A , b = to_least_square(A , b )
-        result = quadprog_solve_qp(A,b)
+            A , b = to_least_square(A , b )
+            result = quadprog_solve_qp(A,b)
 
-        # Law for speed and position of roll / pitch
-        # Speed = min ( - K (P_des - P_cur) , rot_max )
-        rot_max = 0.3  # rad.s-1
-        r_des = - 1.05*np.arctan2(result[1], 1.)
-        p_des = - 1.05*np.arctan2(result[0], 1.)
-        k_sp = 100 
-        epsilon = 0.01
+            self.surface_equation = np.array([result[0] , result[1] , result[2]])
 
-        # print("Desired pitch angle [rad , deg] : " , p_des , " ; " , 57*p_des)
+            # Law for speed and position of roll / pitch
+            # Speed = min ( - K (P_des - P_cur) , rot_max )
+            rot_max = 0.3  # rad.s-1
+            r_des = - 1.*np.arctan2(result[1], 1.)
+            p_des = - 1.*np.arctan2(result[0], 1.)
+            k_sp = 100 
+            epsilon = 0.01
 
-        if r_des != 0 and abs(r_des - RPY[0]) > epsilon :
-            self.referenceStates[3,1:] = r_des
-            self.referenceStates[9,1:] = np.sign(r_des - RPY[0] )*min( rot_max  , k_sp*abs(r_des - RPY[0])  )
-        else : 
-            self.referenceStates[3,1:] = 0.
-            self.referenceStates[9,1:] = 0.
+            # print("Desired pitch angle [rad , deg] : " , p_des , " ; " , 57*p_des)
 
-        if p_des != 0 and abs(p_des - RPY[1]) > epsilon : 
-            self.referenceStates[4,1:] = p_des
-            self.referenceStates[10,1:] = np.sign(p_des - RPY[1] )*min( rot_max  , k_sp*abs(p_des - RPY[1])  )
-        else : 
-            self.referenceStates[4,1:] = 0.
-            self.referenceStates[10,1:] = 0.
+            if r_des != 0 and abs(r_des - RPY[0]) > epsilon :
+                self.referenceStates[3,1:] = r_des
+                self.referenceStates[9,1:] = np.sign(r_des - RPY[0] )*min( rot_max  , k_sp*abs(r_des - RPY[0])  )
+            else : 
+                self.referenceStates[3,1:] = 0.
+                self.referenceStates[9,1:] = 0.
+
+            if p_des != 0 and abs(p_des - RPY[1]) > epsilon : 
+                self.referenceStates[4,1:] = p_des
+                self.referenceStates[10,1:] = np.sign(p_des - RPY[1] )*min( rot_max  , k_sp*abs(p_des - RPY[1])  )
+            else : 
+                self.referenceStates[4,1:] = 0.
+                self.referenceStates[10,1:] = 0.
 
 
-        # Get z
-        isInside , i, j = self.heightMap.find_nearest(q[0, 0:1] , q[1, 0:1])
+            # Get z
+            isInside , i, j = self.heightMap.find_nearest(q[0, 0:1] , q[1, 0:1])
 
-        # if p_des != 0 :
-        #     Z_OFFSET = self.h_ref - abs(p_des*0.05)
+            # if p_des != 0 :
+            #     Z_OFFSET = self.h_ref - abs(p_des*0.05)
+                
+            for k in range(self.n_steps) :
+                isInside , i, j = self.heightMap.find_nearest(self.referenceStates[0,k+1] , self.referenceStates[1,k+1])
+                self.referenceStates[2 , k + 1] = result[0]*self.heightMap.x[i] + result[1]*self.heightMap.y[j] + result[2] + Z_OFFSET   
+
+                if k == 0 : 
+                    self.surface_point = np.array(   [self.heightMap.x[i]  ,self.heightMap.y[j] , result[0]*self.heightMap.x[i] + result[1]*self.heightMap.y[j] + result[2] ]    )   
             
-        for k in range(self.n_steps) :
-            isInside , i, j = self.heightMap.find_nearest(self.referenceStates[0,k+1] , self.referenceStates[1,k+1])
-            self.referenceStates[2 , 1:] = result[0]*self.heightMap.x[i] + result[1]*self.heightMap.y[j] + result[2] + Z_OFFSET      
-        
 
         return 0
 

scripts/solo3D/tools/HeightMap.py
@@ -31,11 +31,12 @@ class HeightMap():
         self.Ny = self.heightMap.shape[1]
         # self.x_bounds = [-2. , 4.]  # object 1 
         # self.y_bounds = [-2. , 1.]
-        self.x_bounds = [-1. , 3.]  # object 4 
-        self.y_bounds = [-2. , 1.]
+        # self.x_bounds = [-1. , 3.]  # object 4 
+        # self.y_bounds = [-2. , 1.]
+        self.x_bounds = [-0.4 , 2.5]  # object 5
+        self.y_bounds = [-2.5, 1.]
         self.x = np.linspace(self.x_bounds[0],self.x_bounds[1],self.Nx)
         self.y = np.linspace(self.y_bounds[0],self.y_bounds[1],self.Ny)
-
 
 
     def load_data(self):

scripts/solo3D/tools/heightMapGenerator.py
@@ -19,17 +19,17 @@ import trimesh
 
 t_init = time.clock()
 
-Nx = 500
-Ny = 300
-x = np.linspace(-1.0,3.,Nx)
-y = np.linspace(-2.0,1.0,Ny)
+Nx = 400
+Ny = 400
+x = np.linspace(-0.4,2.5,Nx)
+y = np.linspace(-2.5,1.,Ny)
 
 # To Edit : 
-object_ = "object_4"
+object_ = "object_5"
 path_object = "solo3D/objects/" + object_ + "/"
 path_mesh = "solo3D/objects/" + object_ + "/meshes/"
 path_saving = "solo3D/objects/" + object_ + "/heightmap/"
-simple_stairs = "simple_stairs_4.stl"   # Full .stl file
+simple_stairs = "stair_rotation_5cm.stl"   # Full .stl file
 
 print("\n Object selected : " , simple_stairs)
 print("Folder for the selected object : " , object_)