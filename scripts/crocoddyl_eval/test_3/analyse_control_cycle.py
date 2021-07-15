0ef33a80d55a79a3d0d2963c48bfb113c0ef6aa2
thomascbrs <thomas.corberes@laposte.net>
2021-07-15 17:59:31 +0100

    Fix mistake, tranform position from log to stop position around in local frame

--- scripts/crocoddyl_eval/test_3/analyse_control_cycle.py
+++ scripts/crocoddyl_eval/test_3/analyse_control_cycle.py
@@ -10,11 +10,12 @@ import matplotlib.pylab as plt
 import libquadruped_reactive_walking as lqrw
 import crocoddyl_class.MPC_crocoddyl_planner as MPC_crocoddyl_planner
 import time 
+import pinocchio as pin
 
 ##############
 #  Parameters
 ##############
-iteration_mpc = 62 # Control cycle
+iteration_mpc = 550 # Control cycle
 Relaunch_DDP = False # Compare a third MPC with != parameters
 linear_mpc = True
 params = lqrw.Params()  # Object that holds all controller parameters
@@ -22,13 +23,15 @@ params = lqrw.Params()  # Object that holds all controller parameters
 ######################
 # Recover Logged data 
 ######################
-file_name = "crocoddyl_eval/logs/data_2021_07_12_09_25.npz"
+file_name = "crocoddyl_eval/logs/data_2021_07_13_11_58.npz"
 logs = np.load(file_name)
 planner_gait = logs.get("planner_gait")
 planner_xref = logs.get("planner_xref")
 planner_fsteps = logs.get("planner_fsteps")
 planner_goals = logs.get("planner_goals")
 mpc_x_f = logs.get("mpc_x_f")
+baseOrientation = logs.get("baseOrientation")
+loop_o_q_int = logs.get("loop_o_q_int")
 
 k = int( iteration_mpc * (params.dt_mpc / params.dt_wbc) ) # simulation iteration corresponding
 k_previous = int( (iteration_mpc - 1) * (params.dt_mpc / params.dt_wbc) ) 
@@ -52,13 +55,24 @@ osqp_us = mpc_osqp.get_latest_result()[12:,:] # Forces computed over the whole p
 mpc_ddp = MPC_crocoddyl_planner.MPC_crocoddyl_planner(params, mu=0.9, inner=False)
 
 # Tune the weights 
-mpc_ddp.heuristicWeights = np.array(4*[0.3, 0.4])
-mpc_ddp.stepWeights = np.full(8, 0.5)
-mpc_ddp.stateWeights = np.sqrt([2.0, 2.0, 20.0, 0.25, 0.25, 10.0, 0.2, 0.2, 0.2, 0.0, 0.0, 0.3]) # fit osqp gains
+# mpc_ddp.heuristicWeights = np.array(4*[0.3, 0.4])
+# mpc_ddp.stepWeights = np.full(8, 0.5)
+# mpc_ddp.stateWeights = np.sqrt([2.0, 2.0, 20.0, 0.25, 0.25, 10.0, 0.2, 0.2, 0.2, 0.0, 0.0, 0.3]) # fit osqp gains
+mpc_ddp.forceWeights = np.array(4*[0.02,0.02,0.02])
+mpc_ddp.stopWeights = np.zeros(8)
 mpc_ddp.initializeModels(params) # re-initialize the model list with the new gains
 
 mpc_ddp.gait = planner_gait[k_previous].copy() # gait_old will be initialised with that
-mpc_ddp.updateProblem(k , planner_xref[k] , planner_fsteps[k], planner_goals[k])
+
+rpy = pin.rpy.matrixToRpy(pin.Quaternion(baseOrientation[k]).toRotationMatrix())
+rpy[0] = 0.
+rpy[1] = 0.
+Rz = pin.rpy.rpyToMatrix(rpy)
+l_stop = Rz.transpose() @ planner_goals[k] 
+for foot in range(4):
+    l_stop[:,foot] += - loop_o_q_int[k,:3]
+
+mpc_ddp.updateProblem(k , planner_xref[k] , planner_fsteps[k], l_stop)
 
 mpc_ddp.ddp.solve(mpc_ddp.x_init,  mpc_ddp.u_init, mpc_ddp.max_iteration)
 
@@ -68,6 +82,8 @@ ddp_us = mpc_ddp.get_latest_result()[12:,:] # Forces computed over the whole pre
 ddp_fsteps = mpc_ddp.get_latest_result()[24:,:]
 ddp_fsteps = np.vstack([planner_fsteps[k,0,:][[0,1,3,4,6,7,9,10]] , ddp_fsteps.transpose()]).transpose() # Add current state 
 
+
+print("Gait : " , mpc_ddp.gait)
 
 #############
 #  Plot     #
